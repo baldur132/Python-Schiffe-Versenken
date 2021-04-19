411c64e85ec93ec3131aebe59308b0ee3897ebe6
baldur <baldur@siegels.us>
2021-04-19 18:55:45 +0200

    fixed game class

--- game.py
+++ game.py
@@ -1,8 +1,11 @@
+""" Primary Game class, ties together the other classes """
+#import ship
+
 class Game:
+    """ Game class, creates a battleship instance """
     def __init__(self, num):
         """
         Game constructor
         :param num: number of players
         """
         self.players_num = num
-    
